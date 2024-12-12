027b510de799938c7c73ddc496c43d9262ca87ca
AsligulSafarova <safarovaasligul98@gmail.com>
2024-12-12 14:54:54 +0100

    added CartDetail

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import style from "../../Styles/Footer.module.scss";
 import logo from "../../assets/Easy.svg";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 export default function Footer() {
   return (
     <div className={style.footer}>
      <img src={logo} alt="" style = {{width:"120px"}}/>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolores est
-         repellat numquam aperiam rem, dolorem officiis consequatur laboriosam possimus non earum, reiciendis delectus.
-         Reiciendis rem esse nam corporis totam.</p>
+         <div style = {{marginLeft:"2rem"}}> 
+          <p style = {{fontSize:"1.7rem", marginBottom:"1rem"}}>© Copyright 2025-2026 |  EasyCook </p>
+          <h2>Created by Asligul Safarova && Houzan </h2>
+         </div>
+         <div  className={style.terms}>
+          <ul >
+            <li>Legal Notice </li>
+            <li>Terms & Conditions  Privacy Policy</li>
+            <li> Cookie Policy</li>
+          </ul>
+         </div>
+         <div className={style.contact}>
+          <h3>Contuct Us</h3>
+          <p> <span><MdEmail />  </span>redischoolproject@gmail.com </p>
+          <p>  <span><FaPhoneAlt /></span>+49785588777  </p>
+         </div>
     </div>
-  )
-}
+  )}
